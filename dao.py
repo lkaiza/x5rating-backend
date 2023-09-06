73fe9a249edd4e5edfe6c382bc432245b8052e16
Vladislav Samoilenko <79825223592@yandex.ru>
2023-09-07 02:55:38 +0300

    db reconnect

--- dao.py
+++ dao.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 import clickhouse_connect
 from dotenv import load_dotenv, find_dotenv
@@ -8,7 +9,14 @@ CLICKHOUSE_HOST = os.environ.get('CLICKHOUSE_HOST')
 CLICKHOUSE_USERNAME = os.environ.get('CLICKHOUSE_USERNAME')
 CLICKHOUSE_PASSWORD = os.environ.get('CLICKHOUSE_PASSWORD')
 
-client = clickhouse_connect.get_client(host=CLICKHOUSE_HOST, username=CLICKHOUSE_USERNAME, password=CLICKHOUSE_PASSWORD)
+client = None
+while client is None:
+    try:
+        client = clickhouse_connect.get_client(host=CLICKHOUSE_HOST, username=CLICKHOUSE_USERNAME,
+                                               password=CLICKHOUSE_PASSWORD)
+    except:
+        time.sleep(3)
+        print("Trying to reconnect...")
 
 
 def get_rows_shops(limit, offset):
